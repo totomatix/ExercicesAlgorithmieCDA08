add0392bd717f3f48bc0b82f350737b35a855e66
WaitInTheBush <theo.ollivier@gmail.com>
2023-10-02 08:48:28 +0200

    Correction de l'exercice 3

---
+++ Exercice3/Program.cs
@@ -0,0 +1,8 @@
+﻿// demander à l'utilisateur le rayon du cercle
+Console.WriteLine("Rayon ?");
+// récupérer le rayon du cercle
+double Rayon = double.Parse(Console.ReadLine());
+// calculer le périmètre
+double Perimetre = 2 * Rayon * Math.PI;
+// affchier le résultat
+Console.WriteLine($"Le cercle de rayon {Rayon} a un périmètre égal à {Perimetre}");
