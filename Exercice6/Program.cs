20dc10bc80cc990ee30a50df5851584716e105e9
WaitInTheBush <theo.ollivier@gmail.com>
2023-10-02 09:08:22 +0200

    Correction Exercice 6

---
+++ Exercice6/Program.cs
@@ -0,0 +1,9 @@
+﻿// demander à l'utilisateur un nombre
+Console.WriteLine("Rentrez un nombre ?");
+// récupérer le rayon du cercle
+int Nombre = int.Parse(Console.ReadLine());
+// si le nombre est pair (c.a.d si il est divisible par 2)
+if (Nombre % 2 == 0)
+{
+    Console.WriteLine($"{Nombre} est pair");
+}
