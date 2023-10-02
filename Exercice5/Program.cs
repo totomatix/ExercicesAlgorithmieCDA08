d56a5f5ce057bb04e9d76f9c53c9d9cd2549abd2
WaitInTheBush <theo.ollivier@gmail.com>
2023-10-02 08:58:30 +0200

    Correction Exercice 5

---
+++ Exercice5/Program.cs
@@ -0,0 +1,17 @@
+﻿int A = 12;
+int B = -5;
+
+// affichage des 2 valeurs avant permutation
+Console.WriteLine("Avant permutation :");
+Console.WriteLine($"A : {A}");
+Console.WriteLine($"B : {B}");
+
+// permutation des valeurs des deux variables
+int Tmp = A;
+A = B;
+B = Tmp;
+
+// affichage des 2 valeurs après permutation
+Console.WriteLine("Après permutation :");
+Console.WriteLine($"A : {A}");
+Console.WriteLine($"B : {B}");
