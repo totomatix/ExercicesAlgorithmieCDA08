bb0201dd2c95e49ba88b2ec09e59797ce4209b52
WaitInTheBush <theo.ollivier@gmail.com>
2023-10-03 11:31:08 +0200

    Corection Exercice10

---
+++ Exercice10/Program.cs
@@ -0,0 +1,16 @@
+﻿// récupération du 1er nombre
+Console.WriteLine("Nombre 1 :");
+int N1 = int.Parse(Console.ReadLine());
+
+// récupération du 2eme nombre
+Console.WriteLine("Nombre 2 :");
+int N2 = int.Parse(Console.ReadLine());
+
+if (N1 >= 0 && N2 >= 0 || N1 <= 0 && N2 <= 0)
+{
+    Console.WriteLine("Positif");
+}
+else
+{
+    Console.WriteLine("Négatif");
+}
