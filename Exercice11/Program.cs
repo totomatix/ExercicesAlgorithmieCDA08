c84a68e14f7084f779518ffd1335a8a40de65ea9
WaitInTheBush <theo.ollivier@gmail.com>
2023-10-03 11:40:55 +0200

    Correction Exercice 11

---
+++ Exercice11/Program.cs
@@ -0,0 +1,21 @@
+﻿// demander un caractère à l'utilisateur
+Console.WriteLine("Rentrez un caractère (o,O pour affirmatif; n,N pour négatif)");
+// récupérer le caractère
+char Caractere = char.Parse(Console.ReadLine());
+// Caractere = Console.ReadKey().KeyChar; autre possibilité
+
+// si 'o' ou 'O' => affirmatif
+if (Caractere == 'o' || Caractere == 'O')
+{
+    Console.WriteLine("Affirmatif");
+}
+// sinon si 'n' ou 'N' => négatif
+else if (Caractere == 'n' || Caractere == 'N')
+{
+    Console.WriteLine("Négatif");
+}
+// dans tous les autre cas => ????
+else
+{
+    Console.WriteLine("???");
+}
