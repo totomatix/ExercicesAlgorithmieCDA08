708fbda47775c5c3e87bd870344a555172f46a13
WaitInTheBush <theo.ollivier@gmail.com>
2023-10-03 09:40:41 +0200

    Correction Exercice 9

---
+++ Exercice9/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Rentrez une note entre 0 et 20 :");
+int Note = int.Parse(Console.ReadLine());
+
+if (Note >=10)
+{
+    Console.WriteLine("Admis");
+}
+else if (Note > 8)
+{
+    Console.WriteLine("Rattrapage");
+}
+else
+{
+    Console.WriteLine("Echec");
+}
