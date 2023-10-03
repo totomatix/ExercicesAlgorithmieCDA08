f1a0aea7028a6c9bbe4ea3da009995675871bcbe
WaitInTheBush <theo.ollivier@gmail.com>
2023-10-03 09:12:33 +0200

    Correction Exercices 7 et 8

---
+++ Exercice7/Program.cs
@@ -0,0 +1,16 @@
+﻿// récupération du 1er réel
+Console.WriteLine("Réel 1 :");
+decimal N1 = decimal.Parse(Console.ReadLine());
+
+// récupération du 2eme réel
+Console.WriteLine("Réel 2 :");
+decimal N2 = decimal.Parse(Console.ReadLine());
+
+if (N1 > N2)
+{
+    Console.WriteLine($"Le nombre le plus grand est : {N1}");
+}
+else
+{
+    Console.WriteLine($"Le nombre le plus grand est : {N2}");
+}
---
+++ Exercice8/Program.cs
@@ -0,0 +1,17 @@
+﻿// récupération du 1er réel
+Console.WriteLine("Nombre entier :");
+int Nombre = int.Parse(Console.ReadLine());
+
+// affichage de : positif, négatif ou nul
+if (Nombre == 0)
+{
+    Console.WriteLine($"Le nombre {Nombre} est nul.");
+}
+else if (Nombre < 0)
+{
+    Console.WriteLine($"Le nombre {Nombre} est négatif.");
+}
+else
+{
+    Console.WriteLine($"Le nombre {Nombre} est positif.");
+}
