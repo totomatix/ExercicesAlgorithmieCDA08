f4761f12263ac0870a0a7fb5ec9dc24d83a88ca8
WaitInTheBush <theo.ollivier@gmail.com>
2023-10-17 15:12:41 +0200

    Coirrections fonctions 4 et 5

--- Fonctions4/Program.cs
+++ Fonctions4/Program.cs
@@ -1,9 +1,40 @@
 ﻿// création de la fonction
-
+// fontion
+// rôle : connaître la mention correspondant à une note
+// nom : Mention
+// retourne : une mention (string)
+// paramètres : la note (int) dont on souhaite connaître la mention
+string Mention(int note)
+{
+    if (note < 0 || note > 20)
+    {
+        return "Erreur : la note doit être entre 0 et 20";
+    }
+    if (note >= 16)
+	{
+		return "Très bien"; 
+	}
+	else if (note >= 14)
+    {
+        return "Bien";
+    }
+    else if (note >= 12)
+    {
+        return "Assez bien";
+    }
+    else if (note >= 10)
+    {
+        return "Passable";
+    }
+    else
+    {
+        return "Echec";
+    }
+}
 
 
 
 // test de la fonction
 
-int note = ...;
+int note = -114;
 Console.WriteLine(Mention(note));
---
+++ Fonctions5/Program.cs
@@ -0,0 +1,30 @@
+﻿// fonction
+// rôle : calculer la distance entre 2 points
+// retourne : distance (double)
+// nom : Distance
+// paramètres : x1, x2, y1, y2 (double)
+double Distance(double x1, double y1, double x2, double y2)
+{
+    double DiffX = x1 - x2;
+    double DiffY = y1 - y2;
+    double DiffX2 = DiffX * DiffX;
+    double DiffY2 = DiffY * DiffY;
+    double Somme = DiffX2 + DiffY2;
+    double Distance = Math.Sqrt(Somme);
+
+    return Distance;
+
+    //return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)); // le calcul sur une seule ligne
+}
+
+
+
+
+// tester la fonction
+double PointAx = 2.3;
+double PointAy = -2.3;
+
+double PointBx = 1;
+double PointBy = -4;
+
+Console.WriteLine(Distance(PointAx,PointAy,PointBx,PointBy));
